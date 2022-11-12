e330471cd2c87866e50b42af5190087a0ebaf02c
Shair <jimmy_jhair17@hotmail.com>
2022-11-12 08:42:54 -0500

    Update api.ts

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -2,4 +2,4 @@ import { isDev } from '../utils/isDev';
 
 export const BASE_URL = isDev
 	? 'http://localhost:3000'
-	: 'https://ea-work-api.shair.dev';
+	: 'https://simple-socialmedia-production.up.railway.app';
